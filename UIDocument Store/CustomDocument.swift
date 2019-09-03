829bca907d812aa113f1230f852c6c4a879d7dd1
Mouzzam <moazzamtahir81@gmail.com>
2019-09-03 23:35:41 +0500

    instantiated subclass of UIDocument class

---
+++ UIDocument Store/CustomDocument.swift
@@ -0,0 +1,13 @@
+//
+//  CustomDocument.swift
+//  UIDocument Store
+//
+//  Created by Moazzam Tahir on 03/09/2019.
+//  Copyright © 2019 Moazzam Tahir. All rights reserved.
+//
+
+import UIKit
+
+class CustomDocument: UIDocument {
+
+}
