50ce138f92aacd4d046f88aeee0106305486ea0c
Mouzzam <moazzamtahir81@gmail.com>
2019-09-04 00:02:24 +0500

    Added New file creation and reading func

--- UIDocument Store/ViewController.swift
+++ UIDocument Store/ViewController.swift
@@ -11,9 +11,42 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var textView: UITextView!
+    
+    var doc: CustomDocument?
+    var urlToDocPath: URL?
+    
+    let fileMng = FileManager.default
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        saveToDoc()
+    }
+    
+    func saveToDoc() {
+        let docsDir = fileMng.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        urlToDocPath = docsDir[0].appendingPathComponent("saveText.txt")
+        if let url = urlToDocPath {
+            doc = CustomDocument(fileURL: url)
+            doc?.userCustomString = ""
+            
+            if fileMng.fileExists(atPath: url.path) {
+                doc?.open(completionHandler: { (success) in
+                    if success {
+                        print("File Opened!")
+                        self.textView.text = self.doc?.userCustomString
+                    }
+                })
+            } else {
+                doc?.save(to: url, for: .forCreating, completionHandler: { (success) in
+                    if success {
+                        print("New file created!")
+                        
+                    }
+                })
+            }
+        }
     }
 
     @IBAction func saveDoc(_ sender: Any) {
